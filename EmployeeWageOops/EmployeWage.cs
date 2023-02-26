2577224c2a6d8419fca51dc8bdab68277a59a1d6
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-26 22:28:40 +0530

    Checking Employee Attendence

---
+++ EmployeeWageOops/EmployeWage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace EmployeeWageOops
+{
+    public class EmployeWage
+    {
+        public static void EmployeeAttende()
+        {
+            Console.WriteLine("Welcome to Employee Wage Program\n ");
+
+            Random random = new Random();
+            int check = random.Next(0,2);
+            if (check == 0)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWageOops/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageOops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeWage.EmployeeAttende();
+        }
+    }
+}
